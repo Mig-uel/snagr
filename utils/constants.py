3cd8696790a76e7779dfe299d980b236c7ce76c8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-22 18:57:46 -0400

    Add LinkedIn job search URL constant
    - Introduces a constant for the LinkedIn job search URL
    - Facilitates easier updates and usage in job scraping processes

---
+++ utils/constants.py
@@ -0,0 +1,2 @@
+# LinkedIn Job Search URL
+SOURCE_URL = "https://www.linkedin.com/jobs/search/?f_TPR=r3600&keywords=junior%20software%20engineer&location=United%20States&origin=JOB_SEARCH_PAGE_JOB_FILTER"
