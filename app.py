d1c9ce33f5017bb803d70d4f63b70f09aab895d8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-22 17:12:15 -0400

    Add "Robert Half" to the list of blacklisted companies
    - Updated the blacklist to include "Robert Half" to prevent
      scraping jobs from this company.

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ jobs = driver.find_elements(By.CLASS_NAME, "base-card")
 jobs_list = []
 
 # blacklisted companies
-companies = ["Lensa", "Wiraa", "Revature", "Mobi.AI"]
+companies = ["Lensa", "Wiraa", "Revature", "Mobi.AI", "Robert Half"]
 blacklisted = {c.lower() for c in companies}
 
 # get existing links from DB
