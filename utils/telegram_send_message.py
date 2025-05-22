a6a39e84f7533e8f43b27085f453036586f0563b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-22 08:06:16 -0400

    Update send_telegram_message function parameters
    - Modify function to accept additional parameters: title, href, and company
    - Enhance flexibility for sending messages with more context

--- utils/telegram_send_message.py
+++ utils/telegram_send_message.py
@@ -6,7 +6,7 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
-def send_telegram_message(message: str):
+def send_telegram_message(message="", title="", href="", company=""):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "HTML"}
 
