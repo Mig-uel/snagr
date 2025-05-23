fe85e55d62c2d7da3b28b6aa31a74b932e81c770
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-22 20:23:20 -0400

    Update LinkedIn job search URL for geoId consistency
    - Changed location parameter to geoId for accurate job search

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,2 @@
 # LinkedIn Job Search URL
-SOURCE_URL = "https://www.linkedin.com/jobs/search/?f_TPR=r3600&keywords=junior%20software%20engineer&location=United%20States&origin=JOB_SEARCH_PAGE_JOB_FILTER"
+SOURCE_URL = "https://www.linkedin.com/jobs/search/?f_TPR=r3600&keywords=junior%20software%20engineer&geoId=103644278&origin=JOB_SEARCH_PAGE_JOB_FILTER"
