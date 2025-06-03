379c43b5656b9ebceba02344b0d97eb778f7a207
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-03 08:26:59 -0400

    Update LinkedIn job search URL for software developer role
    - Changed job keyword from "junior software engineer" to "software developer"
    - Ensures relevance for job searches in the application

--- utils/constants.py
+++ utils/constants.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 # LinkedIn Job Search URL
-SOURCE_URL = "https://www.linkedin.com/jobs/search/?f_TPR=r3600&keywords=junior%20software%20engineer&geoId=103644278&origin=JOB_SEARCH_PAGE_JOB_FILTER"
+SOURCE_URL = "https://www.linkedin.com/jobs/search/?f_TPR=r3600&keywords=software%20developer&geoId=103644278&origin=JOB_SEARCH_PAGE_JOB_FILTER"
 
 # Headless mode varies depending on environment
 HEADLESS = os.getenv("HEADLESS", "True") == "True"
