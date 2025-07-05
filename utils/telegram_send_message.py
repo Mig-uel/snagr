68b67648fa426213f2527053fc438aa4418fa89b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-04 23:15:51 -0400

    Load environment variables in telegram message utility
    - Ensure environment variables are loaded for Telegram bot
    - This allows dynamic configuration of bot token and chat ID

--- utils/telegram_send_message.py
+++ utils/telegram_send_message.py
@@ -1,6 +1,9 @@
 import os
 
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
